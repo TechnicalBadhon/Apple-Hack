0331c4fba403324508a2cd1c325f59194d0612cd
badman <96820962+TechnicalBadhon@users.noreply.github.com>
2025-11-08 03:34:57 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// Your web app's Firebase configuration
+// Replace with your own Firebase config if needed later
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCPX39jxbL-mi6Lo4LmtPXMWn9dq1khX1w",
+  authDomain: "codify09.firebaseapp.com",
+  databaseURL: "https://codify09-default-rtdb.firebaseio.com", // Removed extra spaces
+  projectId: "codify09",
+  storageBucket: "codify09.firebasestorage.app",
+  messagingSenderId: "457920758628",
+  appId: "1:457920758628:web:5f7ffed17caaf6f90cb071",
+  measurementId: "G-LG9RP4RV1E"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
